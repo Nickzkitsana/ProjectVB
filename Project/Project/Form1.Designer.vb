bc95c69c3e6545c760e35187d1d5640d42e30300
Nickz Kitsana <nick7266@hotmail.com>
2019-01-15 23:10:23 +0700

    Update 23.10 : 15/1/62

--- Project/Project/Form1.Designer.vb
+++ Project/Project/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,21 +20,45 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Label5 = New System.Windows.Forms.Label()
         Me.TabPage3 = New System.Windows.Forms.TabPage()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.lblalcohol = New System.Windows.Forms.Label()
+        Me.lblsugar = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.lblpH = New System.Windows.Forms.Label()
+        Me.btnreset = New System.Windows.Forms.Button()
+        Me.btnok = New System.Windows.Forms.Button()
+        Me.txtAlcohol = New System.Windows.Forms.TextBox()
+        Me.txtpH = New System.Windows.Forms.TextBox()
+        Me.txtSugar = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.TabControl1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.TabPage1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -43,140 +67,220 @@ Partial Class Form1
         Me.TabControl1.Controls.Add(Me.TabPage2)
         Me.TabControl1.Controls.Add(Me.TabPage3)
         Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.ImeMode = System.Windows.Forms.ImeMode.NoControl
-        Me.TabControl1.ItemSize = New System.Drawing.Size(50, 25)
-        Me.TabControl1.Location = New System.Drawing.Point(30, 90)
+        resources.ApplyResources(Me.TabControl1, "TabControl1")
         Me.TabControl1.Name = "TabControl1"
-        Me.TabControl1.Padding = New System.Drawing.Point(20, 3)
         Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(642, 321)
-        Me.TabControl1.TabIndex = 0
         '
         'TabPage2
         '
-        Me.TabPage2.Location = New System.Drawing.Point(4, 29)
+        Me.TabPage2.Controls.Add(Me.Label5)
+        resources.ApplyResources(Me.TabPage2, "TabPage2")
         Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(634, 288)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "Details Program"
         Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        resources.ApplyResources(Me.Label5, "Label5")
+        Me.Label5.Name = "Label5"
         '
         'TabPage3
         '
-        Me.TabPage3.Location = New System.Drawing.Point(4, 29)
+        resources.ApplyResources(Me.TabPage3, "TabPage3")
+        Me.TabPage3.Controls.Add(Me.Label12)
+        Me.TabPage3.Controls.Add(Me.Label11)
+        Me.TabPage3.Controls.Add(Me.Label10)
+        Me.TabPage3.Controls.Add(Me.Label9)
+        Me.TabPage3.Controls.Add(Me.Label8)
+        Me.TabPage3.Controls.Add(Me.Label7)
+        Me.TabPage3.Controls.Add(Me.Label6)
+        Me.TabPage3.Controls.Add(Me.PictureBox4)
+        Me.TabPage3.Controls.Add(Me.PictureBox3)
+        Me.TabPage3.Controls.Add(Me.PictureBox2)
+        Me.TabPage3.Controls.Add(Me.PictureBox1)
         Me.TabPage3.Name = "TabPage3"
-        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage3.Size = New System.Drawing.Size(634, 288)
-        Me.TabPage3.TabIndex = 2
-        Me.TabPage3.Text = "Picture"
         Me.TabPage3.UseVisualStyleBackColor = True
         '
-        'Label1
+        'Label12
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 45.0!)
-        Me.Label1.ForeColor = System.Drawing.Color.Gold
-        Me.Label1.Location = New System.Drawing.Point(173, 18)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(371, 69)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Wine Quality"
+        resources.ApplyResources(Me.Label12, "Label12")
+        Me.Label12.Name = "Label12"
+        '
+        'Label11
+        '
+        resources.ApplyResources(Me.Label11, "Label11")
+        Me.Label11.Name = "Label11"
+        '
+        'Label10
+        '
+        resources.ApplyResources(Me.Label10, "Label10")
+        Me.Label10.Name = "Label10"
+        '
+        'Label9
+        '
+        resources.ApplyResources(Me.Label9, "Label9")
+        Me.Label9.Name = "Label9"
+        '
+        'Label8
+        '
+        resources.ApplyResources(Me.Label8, "Label8")
+        Me.Label8.Name = "Label8"
+        '
+        'Label7
+        '
+        resources.ApplyResources(Me.Label7, "Label7")
+        Me.Label7.Name = "Label7"
+        '
+        'Label6
+        '
+        resources.ApplyResources(Me.Label6, "Label6")
+        Me.Label6.Name = "Label6"
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.Project.My.Resources.Resources.ภาพ2
+        resources.ApplyResources(Me.PictureBox4, "PictureBox4")
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.Project.My.Resources.Resources.ตาราง2
+        resources.ApplyResources(Me.PictureBox3, "PictureBox3")
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.Project.My.Resources.Resources.ตาราง1
+        resources.ApplyResources(Me.PictureBox2, "PictureBox2")
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Project.My.Resources.Resources.กราฟ
+        resources.ApplyResources(Me.PictureBox1, "PictureBox1")
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.TabStop = False
         '
         'TabPage1
         '
-        Me.TabPage1.BackColor = System.Drawing.SystemColors.ControlText
+        Me.TabPage1.BackColor = System.Drawing.Color.Transparent
+        Me.TabPage1.Controls.Add(Me.lblalcohol)
+        Me.TabPage1.Controls.Add(Me.lblsugar)
         Me.TabPage1.Controls.Add(Me.Button1)
-        Me.TabPage1.Controls.Add(Me.TextBox3)
-        Me.TabPage1.Controls.Add(Me.TextBox2)
-        Me.TabPage1.Controls.Add(Me.TextBox1)
+        Me.TabPage1.Controls.Add(Me.lblpH)
+        Me.TabPage1.Controls.Add(Me.btnreset)
+        Me.TabPage1.Controls.Add(Me.btnok)
+        Me.TabPage1.Controls.Add(Me.txtAlcohol)
+        Me.TabPage1.Controls.Add(Me.txtpH)
+        Me.TabPage1.Controls.Add(Me.txtSugar)
         Me.TabPage1.Controls.Add(Me.Label4)
         Me.TabPage1.Controls.Add(Me.Label3)
         Me.TabPage1.Controls.Add(Me.Label2)
-        Me.TabPage1.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.TabPage1.Location = New System.Drawing.Point(4, 29)
+        Me.TabPage1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        resources.ApplyResources(Me.TabPage1, "TabPage1")
         Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(634, 288)
-        Me.TabPage1.TabIndex = 3
-        Me.TabPage1.Text = "Program"
         '
-        'Label2
+        'lblalcohol
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
-        Me.Label2.Location = New System.Drawing.Point(44, 37)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(94, 20)
-        Me.Label2.TabIndex = 0
-        Me.Label2.Text = "ปริมาณน้ำตาล"
+        resources.ApplyResources(Me.lblalcohol, "lblalcohol")
+        Me.lblalcohol.ForeColor = System.Drawing.Color.Red
+        Me.lblalcohol.Name = "lblalcohol"
         '
-        'Label3
+        'lblsugar
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
-        Me.Label3.Location = New System.Drawing.Point(88, 94)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(50, 20)
-        Me.Label3.TabIndex = 1
-        Me.Label3.Text = "ค่า pH"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
-        Me.Label4.Location = New System.Drawing.Point(57, 153)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(81, 20)
-        Me.Label4.TabIndex = 2
-        Me.Label4.Text = "ค่า Alcohol"
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(190, 37)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(184, 20)
-        Me.TextBox1.TabIndex = 3
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(190, 96)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(184, 20)
-        Me.TextBox2.TabIndex = 4
-        '
-        'TextBox3
-        '
-        Me.TextBox3.Location = New System.Drawing.Point(190, 153)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(184, 20)
-        Me.TextBox3.TabIndex = 5
+        resources.ApplyResources(Me.lblsugar, "lblsugar")
+        Me.lblsugar.ForeColor = System.Drawing.Color.Red
+        Me.lblsugar.Name = "lblsugar"
         '
         'Button1
         '
-        Me.Button1.BackColor = System.Drawing.Color.Lime
-        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
-        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Button1.BackColor = System.Drawing.Color.MintCream
+        resources.ApplyResources(Me.Button1, "Button1")
         Me.Button1.ForeColor = System.Drawing.Color.Black
-        Me.Button1.Location = New System.Drawing.Point(405, 225)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(207, 44)
-        Me.Button1.TabIndex = 6
-        Me.Button1.Text = "Show Wine Quality"
         Me.Button1.UseVisualStyleBackColor = False
+        '
+        'lblpH
+        '
+        resources.ApplyResources(Me.lblpH, "lblpH")
+        Me.lblpH.ForeColor = System.Drawing.Color.Red
+        Me.lblpH.Name = "lblpH"
+        '
+        'btnreset
+        '
+        Me.btnreset.BackColor = System.Drawing.Color.MintCream
+        resources.ApplyResources(Me.btnreset, "btnreset")
+        Me.btnreset.ForeColor = System.Drawing.Color.Black
+        Me.btnreset.Name = "btnreset"
+        Me.btnreset.UseVisualStyleBackColor = False
+        '
+        'btnok
+        '
+        Me.btnok.BackColor = System.Drawing.Color.MintCream
+        resources.ApplyResources(Me.btnok, "btnok")
+        Me.btnok.ForeColor = System.Drawing.Color.Black
+        Me.btnok.Name = "btnok"
+        Me.btnok.UseVisualStyleBackColor = False
+        '
+        'txtAlcohol
+        '
+        resources.ApplyResources(Me.txtAlcohol, "txtAlcohol")
+        Me.txtAlcohol.Name = "txtAlcohol"
+        '
+        'txtpH
+        '
+        resources.ApplyResources(Me.txtpH, "txtpH")
+        Me.txtpH.Name = "txtpH"
+        '
+        'txtSugar
+        '
+        resources.ApplyResources(Me.txtSugar, "txtSugar")
+        Me.txtSugar.Name = "txtSugar"
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Name = "Label4"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.ForeColor = System.Drawing.Color.DarkGoldenrod
+        Me.Label1.Name = "Label1"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
         Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
-        Me.ClientSize = New System.Drawing.Size(700, 451)
+        Me.BackgroundImage = Global.Project.My.Resources.Resources._431793
+        resources.ApplyResources(Me, "$this")
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
         Me.Name = "Form1"
-        Me.Text = "Wine"
         Me.TabControl1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.TabPage3.ResumeLayout(False)
+        Me.TabPage3.PerformLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.TabPage1.ResumeLayout(False)
         Me.TabPage1.PerformLayout()
         Me.ResumeLayout(False)
@@ -189,11 +293,28 @@ Partial Class Form1
     Friend WithEvents TabPage3 As TabPage
     Friend WithEvents Label1 As Label
     Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents Button1 As Button
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnok As Button
+    Friend WithEvents txtAlcohol As TextBox
+    Friend WithEvents txtpH As TextBox
+    Friend WithEvents txtSugar As TextBox
     Friend WithEvents Label4 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents btnreset As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents lblpH As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lblalcohol As Label
+    Friend WithEvents lblsugar As Label
 End Class
--- Project/Project/Form1.vb
+++ Project/Project/Form1.vb
@@ -1,3 +1,115 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnok.Click
+        Dim txt As String = ""
+        Dim Sugar As Double
+        Dim pH As Double
+        Dim Alcohol As Double
 
+        Try
+            Sugar = CDbl(txtSugar.Text)
+            pH = CDbl(txtpH.Text)
+            Alcohol = CDbl(txtAlcohol.Text)
+
+            If Double.TryParse(txtSugar.Text, 0) = False Then
+                MessageBox.Show("กรุณากรอกปริมาณของน้ำตาลเป็นตัวเลข" & vbNewLine & "Ex. 0-999", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                txtSugar.Text = ""
+                txtpH.Text = ""
+                txtAlcohol.Text = ""
+                Exit Sub
+            ElseIf Double.TryParse(txtpH.Text, 0) = False Then
+                MessageBox.Show("กรุณากรอกค่า pH เป็นตัวเลข" & vbNewLine & "Ex. 0-999", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                txtSugar.Text = ""
+                txtpH.Text = ""
+                txtAlcohol.Text = ""
+                Exit Sub
+            ElseIf Double.TryParse(txtAlcohol.Text, 0) = False Then
+                MessageBox.Show("กรุณากรอกค่า Alcohol เป็นตัวเลข" & vbNewLine & "Ex. 0-999", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                txtSugar.Text = ""
+                txtpH.Text = ""
+                txtAlcohol.Text = ""
+                Exit Sub
+            End If
+
+            If Alcohol <= 10.5 Then
+                If Sugar <= 9.5 Then
+                    txt = "Low"
+                Else
+                    txt = "Mid"
+                End If
+            ElseIf Alcohol >= 10.6 Or Alcohol <= 12.5 Then
+                If pH <= 3.5 Then
+                    If Sugar <= 2.5 Then
+                        txt = "Mid"
+                    Else
+                        txt = "Low"
+                    End If
+                End If
+            End If
+
+            If Alcohol > 12.5 Then
+                MessageBox.Show("ปริมาณ Alcohol ควรน้อยกว่าหรือเท่ากับ 12.5", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                txtAlcohol.Text = ""
+            ElseIf pH > 3.5 Then
+                MessageBox.Show("ปริมาณ pH ควรน้อยกว่าหรือเท่ากับ 3.5", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                txtpH.Text = ""
+            ElseIf Sugar > 100 Then
+                MessageBox.Show("ปริมาณของน้ำตาล ควรน้อยกว่า 100", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                txtSugar.Text = ""
+            Else
+                MessageBox.Show("ปริมาณของน้ำตาล :  " & txtSugar.Text & vbNewLine &
+                        "ค่า pH  " & txtpH.Text & vbNewLine &
+                        "ค่า Alcohol  " & txtAlcohol.Text & vbNewLine &
+                        "คุณภาพระดับ  " & txt, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                txtSugar.Text = ""
+                txtpH.Text = ""
+                txtAlcohol.Text = ""
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Error with : " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnreset.Click
+        txtSugar.Text = ""
+        txtpH.Text = ""
+        txtAlcohol.Text = ""
+        btnok.Enabled = False
+        btnreset.Enabled = False
+        btnok.BackColor = Color.Bisque
+        btnreset.BackColor = Color.Bisque
+    End Sub
+
+    Private Sub txtSugar_TextChanged(sender As Object, e As EventArgs) Handles txtSugar.TextChanged, txtpH.TextChanged, txtAlcohol.TextChanged
+
+        If txtAlcohol.Text <> "" And txtpH.Text <> "" And txtSugar.Text <> "" Then
+            btnok.Enabled = True
+            btnok.BackColor = Color.Lime
+        ElseIf txtAlcohol.Text <> "" Or txtpH.Text <> "" Or txtSugar.Text <> "" Then
+            btnreset.Enabled = True
+            btnreset.BackColor = Color.PaleVioletRed
+        Else
+            btnok.Enabled = False
+            btnreset.Enabled = False
+            btnok.BackColor = Color.Bisque
+            btnreset.BackColor = Color.Bisque
+        End If
+
+    End Sub
+
+    Dim btnClicked As Boolean = False
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        If btnClicked = True Then
+            lblpH.Text = ""
+            lblsugar.Text = ""
+            lblalcohol.Text = ""
+            btnClicked = False
+        Else
+            lblpH.Text = "ค่า pH ควรน้อยกว่าหรือเท่ากับ 3.5"
+            lblalcohol.Text = "ค่า Alcohol ควรน้อยกว่าหรือเท่ากับ 12.5"
+            lblsugar.Text = "ค่าปริมาณน้ำตาล ควรน้อยกว่าหรือเท่ากับ 100"
+            btnClicked = True
+        End If
+    End Sub
 End Class
